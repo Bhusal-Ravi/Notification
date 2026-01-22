50074a2605257f0e15e273950ee7873d844a345d
Bhusal-Ravi <ravidonbhusal@gmail.com>
2026-01-22 14:44:14 +0545

    Queue, Worker and Job Schedular added Postgress Database Schema User, Task, TaskUser Link with Many to Many Relation Bullmq, Redis as packages

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,16 +1,15 @@
 import express from 'express'
 const router= express.Router()
 import { client } from '../config/dbConnection.js'
+import { notificationQueue } from '../server.js'
 
 
 
 
 router.get('/test', async (req,res)=>{
     try{
-         const user= await  client.query('Select * from UserInfo')
-         if(user){
-            console.log(user)
-         }
+        await notificationQueue.add('test123',{message:"Random test job"})
+        res.json("New message incoming wait 5 sec")
     }catch(error){
         console.log('test route error:',error)
     }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { dbConnect } from './config/dbConnection.js'
 import testRoute from './routes/test.js'
+import { Queue, Worker } from 'bullmq'
 dotenv.config()
 
 
@@ -9,9 +10,57 @@ const port=3000
 const app= express()
 dbConnect()
 
+// Setup Queue and Worker
+const connection = {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+};
+
+let count = 0;
+
+
+
+export const notificationQueue = new Queue('hello', { connection });
+export const notificationWorker = new Worker('hello', async job => {
+    count++;
+    console.log(`Count: ${count}`, job.data, job.name);
+}, { connection,
+    removeOnFail:{count:100},
+    removeOnComplete:{count:10},
+    concurrency:5,
+    limiter:{
+        max:10,
+        duration:1000
+    }
+});
+
+notificationWorker.on('completed', job => {
+    console.log(`Job completed: ${job.name} (${job.id})`);
+});
+
+notificationWorker.on('failed', (job, err) => {
+    console.error(`Job failed: ${job?.name} (${job?.id}) -> ${err.message}`);
+});
+
+notificationWorker.on('error', err => {
+    console.error('Worker error:', err);
+});
+
+// Setup recurring job
+await notificationQueue.upsertJobScheduler(
+    'repeatingJob',
+    {
+        pattern: '* * * * *', // Every minute
+    },
+    {
+        name: 'repeat',
+        data: { message: 'Recurring notification check' }
+    }
+);
+
+
 
 app.use('/api',testRoute)
-
 
 
 
