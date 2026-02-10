06767c7510a1145bac417a6667ca45cd196144a8
Bhusal-Ravi <ravidonbhusal@gmail.com>
2026-02-10 20:20:26 +0545

    refactor(Login): simplify login user function and clean up callback URLs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,14 +9,13 @@ import {authClient} from '../../lib/auth-client'
 function Login() {
 
   async function LoginUser(){
-    console.log("....")
+   
    try{ 
-    const frontendOrigin = (import.meta.env.VITE_APP_URL ?? window.location.origin).replace(/\/$/, '')
     await authClient.signIn.social({
       provider:'google',
-      callbackURL:`${frontendOrigin}/main`,
+      callbackURL:'/main',
       
-      newUserCallbackURL:`${frontendOrigin}/welcome`,
+      newUserCallbackURL:'/welcome',
        disableRedirect: false,
     })
     }catch(error){
@@ -25,11 +24,11 @@ function Login() {
   }
 
   return (
-    <div>
-      <button onClick={LoginUser}>
+    <button onClick={LoginUser} className='bg-black rounded-md'>
+      <span className='bg-[#ffff00] block px-4 py-2 -translate-x-1 -translate-y-1 border-black border-2 rounded-md text-sm font-semibold hover:-translate-y-2 hover:-translate-x-2 active:translate-x-0 active:translate-y-0 transition-all'>
         Login With Google
-      </button>
-    </div>
+      </span>
+    </button>
   )
 }
 
